4a95dfb61e9b0427f7481945623fdcda132caf69
m0vls <101503113+m0vls@users.noreply.github.com>
2024-12-18 11:31:28 +0500

    Add files via upload

---
+++ launcher/launcher/MainMenu.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Diagnostics;
+
+
+namespace launcher
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void goToSettings_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow.NavigateToSettings();
+        }
+
+        private void goToStatistics_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow.NavigateToStatistics();
+        }
+
+        private void launchGame_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string exePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TDGame.exe");
+
+                // Проверка существует ли файл
+                if (File.Exists(exePath))
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = exePath,
+                        UseShellExecute = true
+                    });
+                }
+                else
+                {
+                    MessageBox.Show("Файл игры не найден.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при запуске игры: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ launcher/launcher/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+//using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+
+
+namespace launcher
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string companyName = "GameBakery";
+        static string gameName = "TDGame";
+
+        static string persistentDataPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "..", "LocalLow", companyName, gameName);
+
+        public static string statsFilePath = Path.Combine(persistentDataPath, "statistics.json");
+
+        public GameData gameData { get; set; }
+
+        private MainMenu _menu;
+        private settings _settings;
+        private statistics _statistics;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+            if (!Directory.Exists(persistentDataPath))
+            {
+                Directory.CreateDirectory(persistentDataPath);
+            }
+
+            gameData = LoadGameData(statsFilePath);
+            //kill.Text = $"Kills: {gameData.kills}";
+
+            _menu = new MainMenu();
+            _settings = new settings(gameData);
+            _statistics = new statistics(gameData);
+
+            Menu.Navigate(_menu);
+        }
+        public void NavigateToSettings()
+        {
+            Menu.Navigate(_settings);
+        }
+
+        public void NavigateToStatistics()
+        {
+            Menu.Navigate(_statistics);
+        }
+
+        private GameData LoadGameData(string path)
+        {
+            if (!File.Exists(path))
+            {
+                // Если файла не существует, создаём с начальными значениями
+                var defaultData = new GameData();
+                SaveGameData(path, defaultData);
+                return defaultData;
+            }
+
+            try
+            {
+                //Загрузка файла
+                string json = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<GameData>(json) ?? new GameData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при чтении файла: {ex.Message}");
+                return new GameData(); // Возвращаем объект по умолчанию в случае ошибки
+            }
+        }
+
+        public static void SaveGameData(string path, GameData data)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+                File.WriteAllText(path, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении файла: {ex.Message}");
+            }
+        }
+
+    }
+    public class GameData
+    {
+        public int kills;
+        public int deaths;
+        public int height;
+        public int width;
+        public bool fullscreen;
+
+        public GameData()
+        {
+            kills = 0;
+            deaths = 0;
+            height = 1080;
+            width = 1920;
+            fullscreen = true;
+        }
+    }
+}
